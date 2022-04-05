1739553cc5942fc5716395ca8d9bbf1eb41b9b91
Nicolas Hennebert <nicolas.hbt@protonmail.com>
2022-04-05 11:27:18 +0200

    add text to about and home

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,25 @@
 function About() {
   return (
-    <div>About</div>
-  )
+    <div>
+      <h3 className="text-3xl">About</h3>
+      <p>
+        To make this application, I used React.JS, Tailwind css and Daisy UI and
+        firebase for the backend. You may be wondering why I didn't make the
+        backend of this app. The reason is simple, I wanted to have a live
+        version of this project and hosting a .NET backend can get expensive. If
+        you have a look at one of my back-ends: I have already made a fully
+        functional REST API for a different project, you can find the code&nbsp;
+        <a
+          className="link"
+          href="https://github.com/nicolashbt/musium"
+          target="_blank"
+        >
+          here
+        </a>
+        .
+      </p>
+    </div>
+  );
 }
 
-export default About
+export default About;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,26 @@
 function Home() {
   return (
     <div>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga, obcaecati? Eius enim eveniet, aliquid cumque unde sunt non rerum eaque, deserunt quae, ad excepturi modi natus voluptate corporis fugiat pariatur?
-      </p>
+      <h3 className="text-3xl">Home</h3>
+      <p>Welcome to my website, I started this project for a few reasons:</p>
+      <ol className="list-decimal list-inside bg-base-200">
+        <li>I want to improve my skills with React and Javascript</li>
+        <li>I want to keep coding while I'm doing interviews</li>
+        <li>I want to show my future employers what I can do</li>
+      </ol>
+      <p>On this website, you can find a few pages. There is:</p>
+      <ul className="list-disc list-inside bg-base-200">
+        <li>A home page which you are on right now.</li>
+        <li>A marketplace, a listing of ads for musicians</li>
+        <li>A contact page in case you want to get in touch</li>
+        <li>An about page showing the code, the technologies used, etc.</li>
+        <li>
+          Authentication pages if you want to try all the features of the
+          marketplace
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
