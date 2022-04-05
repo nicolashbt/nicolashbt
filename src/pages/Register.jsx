c59f99e58539a3711078db38bef88286989e7f80
Nicolas Hennebert <nicolas.hbt@protonmail.com>
2022-04-05 11:15:02 +0200

    add form

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,49 @@
+import { Link } from "react-router-dom";
+import { FaAngleRight } from "react-icons/fa";
+
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <div className="hero">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h3 className="text-3xl font-bold">New account</h3>
+        </div>
+        <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-200">
+          <div className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="text"
+                placeholder="email"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="text"
+                placeholder="password"
+                className="input input-bordered"
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">Register</button>
+            </div>
+            <div>
+              <Link to="/login" className="link link-secondary">
+                Login Instead
+                <FaAngleRight className="inline" />
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
