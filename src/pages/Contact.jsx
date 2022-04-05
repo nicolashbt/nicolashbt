5c5913f30f1b582aa31056094a7ad35d0496d6fc
Nicolas Hennebert <nicolas.hbt@protonmail.com>
2022-04-05 11:14:37 +0200

    add text and links

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,48 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { ImMail4 } from "react-icons/im";
+
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div>
+      <h3 className="text-3xl">Contact</h3>
+      <p>
+        I'm Nicolas Hennebert, I come from and currently live in Belgium. I
+        started programming in September 2021. I followed an intensive training
+        at Technofutur TIC. I learned all the technologies needed to become a
+        Full Stack .NET developer with Angular. After some time spent looking at
+        job ads and talking to fellow developers, I decided to learn JavaScript
+        and React.
+      </p>
+      <div>
+        <ul>
+          <li>
+            <a href="mailto:nicolas.hbt@protonmail.com" target="_blank">
+              <button className="btn btn-sm btn-ghost">
+                <ImMail4 className="inline text-xl" />
+                &nbsp;nicolas.hbt@protonmail.com
+              </button>
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/nicolashbt" target="_blank">
+              <button className="btn btn-sm btn-ghost">
+                <FaGithub className="inline text-xl" />
+                &nbsp;/nicolashbt
+              </button>
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/nicolashbt/" target="_blank">
+              <button className="btn btn-sm btn-ghost">
+                <FaLinkedin className="inline text-xl" />
+                &nbsp;/nicolashbt
+              </button>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
