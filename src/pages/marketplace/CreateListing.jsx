183297bcf7323e778baf289c08c44342b41f0b5e
Nicolas Hennebert <nicolas.hbt@protonmail.com>
2022-04-11 15:55:21 +0200

    add form

--- src/pages/marketplace/CreateListing.jsx
+++ src/pages/marketplace/CreateListing.jsx
@@ -24,11 +24,23 @@ function CreateListing() {
     location: "",
     handDelivered: true,
     price: 0,
-    used: true,
     images: {},
     latitude: 0,
     longitude: 0,
   });
+
+  const {
+    type,
+    name,
+    category,
+    description,
+    price,
+    handDelivered,
+    location,
+    images,
+    latitude,
+    longitude,
+  } = formData;
 
   const auth = getAuth();
   const navigate = useNavigate();
@@ -53,7 +65,172 @@ function CreateListing() {
     return <Spinner />;
   }
 
-  return <div>CreateListing</div>;
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(formData);
+  };
+  const onMutate = (e) => {
+    let boolean = null;
+    if (e.target.value === "true") {
+      boolean = true;
+    }
+    if (e.target.value === "false") {
+      boolean = false;
+    }
+    if (e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        images: e.target.files,
+      }));
+    }
+    if (!e.target.files) {
+      setFormData((prevState) => ({
+        ...prevState,
+        [e.target.id]: boolean ?? e.target.value,
+      }));
+    }
+  };
+
+  return (
+    <div className="">
+      <header>
+        <p className="text-3xl">Create a Listing</p>
+      </header>
+
+      <main>
+        <form onSubmit={onSubmit}>
+          <div className="form-control w-full max-w-s">
+            <label className="label">Name </label>
+            <input
+              type="text"
+              placeholder=""
+              className="input input-bordered w-full"
+              id="name"
+              onChange={onMutate}
+              maxLength="32"
+              minLength="3"
+              value={name}
+              required
+            />
+
+            <label className="label mt-3">Type</label>
+            <div className="btn-group">
+              <button
+                className={
+                  type === "hardware" ? "btn btn-active btn-sm" : "btn btn-sm"
+                }
+                type="button"
+                id="type"
+                value={"hardware"}
+                onClick={onMutate}
+              >
+                Hardware
+              </button>
+              <button
+                className={
+                  type === "software" ? "btn btn-active btn-sm" : "btn btn-sm"
+                }
+                type="button"
+                id="type"
+                value={"software"}
+                onClick={onMutate}
+              >
+                Software
+              </button>
+            </div>
+
+            <label className="label mt-3">Description</label>
+            <textarea
+              className="textarea"
+              placeholder=""
+              id="description"
+              value={description}
+              onChange={onMutate}
+            ></textarea>
+
+            <label className="label mt-3">Price</label>
+            <input
+              type="number"
+              placeholder=""
+              className="input input-bordered w-full max-w-xs"
+              id="price"
+              min="0.00"
+              max="10000.00"
+              step="0.01"
+              onChange={onMutate}
+              required
+              value={price}
+            />
+
+            <label className="label mt-3">Location</label>
+            <input
+              type="text"
+              placeholder=""
+              className="input input-bordered w-full"
+              id="location"
+              onChange={onMutate}
+              maxLength="32"
+              minLength="4"
+              required
+              value={location}
+            />
+
+            <label className="label mt-3">Pick up only</label>
+            <div className="btn-group">
+              <button
+                className={
+                  handDelivered === true
+                    ? "btn btn-active btn-sm"
+                    : "btn btn-sm"
+                }
+                type="button"
+                id="handDelivered"
+                value={"true"}
+                onClick={onMutate}
+              >
+                Yes
+              </button>
+              <button
+                className={
+                  handDelivered === false
+                    ? "btn btn-active btn-sm"
+                    : "btn btn-sm"
+                }
+                type="button"
+                id="handDelivered"
+                value={"false"}
+                onClick={onMutate}
+              >
+                No
+              </button>
+            </div>
+
+            <label className="label mt-3">Images</label>
+            <p className="">The first image will be the cover (max 5).</p>
+            <input
+              className="block w-full text-sm text-white
+              file:mr-4 file:py-2 file:px-4
+              file:rounded-full file:border-0
+              file:text-sm file:font-semibold
+              file:bg-base-100 file:text-white
+              hover:file:bg-primary"
+              type="file"
+              id="images"
+              onChange={onMutate}
+              max="6"
+              accept=".jpg,.png,.jpeg"
+              multiple
+              required
+            />
+          </div>
+
+          <button type="submit" className="btn btn-primary mt-6">
+            Create Listing
+          </button>
+        </form>
+      </main>
+    </div>
+  );
 }
 
 export default CreateListing;
